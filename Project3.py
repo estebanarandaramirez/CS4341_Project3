8b8637e284390fda3d1d5e3a71cc89ce0e512dee
unknown <esteban.arandaramirez@gmail.com>
2019-09-15 22:28:24 -0400

    Excel for random forest

--- Project3.py
+++ Project3.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import KFold
 from sklearn import metrics
 
 def DecisionTree(X, Y):
+    global maxColumn
     decisionTree = DecisionTreeClassifier(criterion='entropy')
     kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
     cvscores = []
@@ -16,9 +17,12 @@ def DecisionTree(X, Y):
         decisionTree = decisionTree.fit(X[train], Y[train])
         Y_pred = decisionTree.predict(X[test])
         cvscores.append(metrics.accuracy_score(Y[test], Y_pred) * 100)
-    print("Dtree - Mean(+/-Standard Deviation): %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
-    print("Dtree - Final test accuracy: %.2f%%" % (metrics.accuracy_score(Y[test], Y_pred) * 100))
-    with open('results.csv', 'a', newline='') as file:
+        df = pd.read_csv(secondInputCSV)
+        df['new_column'] = 'hi'
+        df.to_csv(outputFilePath)
+    print("DTree - Mean(+/-Standard Deviation): %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+    print("DTree - Final test accuracy: %.2f%%" % (metrics.accuracy_score(Y[test], Y_pred) * 100))
+    with open('DTresults.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow((' ', np.mean(cvscores), np.std(cvscores), metrics.accuracy_score(Y[test], Y_pred) * 100))
 
@@ -31,37 +35,55 @@ def RandomForest(X, Y):
         rTree = rTree.fit(X[train], Y[train].ravel())
         Y_pred = rTree.predict(X[test])
         cvscores.append(metrics.accuracy_score(Y[test], Y_pred) * 100)
-    print("Rforest - Mean(+/-Standard Deviation): %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
-    print("Rforest - Final test accuracy: %.2f%%" % (metrics.accuracy_score(Y[test], Y_pred) * 100))
+    print("RForest - Mean(+/-Standard Deviation): %.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+    print("RForest - Final test accuracy: %.2f%%" % (metrics.accuracy_score(Y[test], Y_pred) * 100))
+    with open('RFresults.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow((' ', np.mean(cvscores), np.std(cvscores), metrics.accuracy_score(Y[test], Y_pred) * 100))
 
 
 seed = np.random.seed(1)
+maxColumn = 42
 
 if len(sys.argv) != 3:
     sys.exit("Must specify input and output files")
-firstInputCSV = sys.argv[1]
-secondInputCSV = sys.argv[2]
 
-with open('results.csv', 'w', newline='') as file:
+firstInputCSV = sys.argv[1]
+loadInput = np.loadtxt(firstInputCSV, delimiter=',', dtype='str', skiprows=1)
+
+secondInputCSV = sys.argv[2]
+outputFilePath = "./" + str(secondInputCSV)
+with open(outputFilePath, 'w', newline='') as outputFile:
+    df = pd.read_csv(firstInputCSV)
+    df.to_csv(outputFile, index=False)
+
+with open('DTresults.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow((' ', 'Mean', 'STD', 'Final test accuracy'))
+with open('RFresults.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow((' ', 'Mean', 'STD', 'Final test accuracy'))
 
-loadInput = np.loadtxt(firstInputCSV, delimiter=',', dtype='str', skiprows=1)
 features = getFeatures(loadInput)
-
 Y = features[10].values.reshape(-1, 1)
 for i in range (10):
     X = features[i].values.reshape(-1, 1)
-    with open('results.csv', 'a', newline='') as file:
+    with open('DTresults.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Feature %i:' % i])
+        writer.writerow(['Feature %i:' % (i+1)])
+    with open('RFresults.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Feature %i:' % (i+1)])
     print("Feature #%i:" % (i+1))
     DecisionTree(X, Y)
     RandomForest(X, Y)
 
 print()
 
-with open('results.csv', 'a', newline='') as file:
+with open('DTresults.csv', 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['All features:'])
+with open('RFresults.csv', 'a', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['All features:'])
 print("All features combined:")
@@ -75,15 +97,12 @@ print()
 for i in range (10):
     featuresToTest = features.loc[:, features.columns != i]
     X = features[i].values.reshape(-1, 1)
-    with open('results.csv', 'a', newline='') as file:
+    with open('DTresults.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['All except %i:' % i])
+        writer.writerow(['All except %i:' % (i+1)])
+    with open('RFresults.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['All except %i:' % (i+1)])
     print("All features except #%i:" % (i+1))
     DecisionTree(X, Y)
     RandomForest(X, Y)
-
-outputFilePath = "./" + str(secondInputCSV)
-with open(outputFilePath, 'w', newline='') as outputFile:
-    # writer = csv.writer(outputFile, delimiter=',')
-    df = pd.read_csv(firstInputCSV)
-    df.to_csv(outputFile, index=False)
